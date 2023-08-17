7eec488ca34b27f1a43a1a788426fdf272649cdf
Roy Millan <millan.rodrigo201@gmail.com>
2023-08-17 17:57:52 -0600

    encrypt password in create and update method

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -5,12 +5,19 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-    
+
     def create(self, validated_data):
         user = User(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        updated_user = super().update(instance, validated_data)
+        updated_user.set_password(validated_data['password'])
+        updated_user.save()
+        return updated_user
+
 
 class UserListSerializer(serializers.ModelSerializer):
     class Meta:
