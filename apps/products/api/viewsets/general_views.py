7d84d6f76b96a2e0dddc9e347b01c32bb77d1615
Roy Millan <millan.rodrigo201@gmail.com>
2023-08-17 19:29:45 -0600

    Create views generic ListAPIView

---
+++ apps/products/api/viewsets/general_views.py
@@ -0,0 +1,26 @@
+from rest_framework import generics
+
+from apps.products.models import MeasureUnit, Indicator, CategoryProduct
+from apps.products.api.serializers.general_serializers import (
+    MeasureUnitSerializer, IndicatorSerializer, CategoryProductSerializer
+)
+
+class MeasureUnitListAPIView(generics.ListAPIView):
+    serializer_class = MeasureUnitSerializer
+
+    # multi-Get
+    def get_queryset(self):
+        return MeasureUnit.objects.filter(state=True)
+    
+class IndicatorListAPIView(generics.ListAPIView):
+    serializer_class = IndicatorSerializer
+
+    # multi-Get
+    def get_queryset(self):
+        return Indicator.objects.filter(state=True)
+    
+class CategoryProductListAPIView(generics.ListAPIView):
+    serializer_class = CategoryProductSerializer
+
+    def get_queryset(self):
+        return CategoryProduct.objects.filter(state=True)
