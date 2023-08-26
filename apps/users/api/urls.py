8646c797509453c824c03e0dab8c0ad70cf84d49
Roy Millan <millan.rodrigo201@gmail.com>
2023-08-26 15:32:16 -0600

    21.-  RetrieveAPIView y DestroyAPIView

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -3,5 +3,5 @@ from apps.users.api.api import user_api_view, user_detail_api_view
 
 urlpatterns = [
     path('usuario/', user_api_view, name='usuario_api'),
-    path('usuario/<int:pk>', user_detail_api_view, name='usuario_detail_api_view'),
+    path('usuario/<int:pk>/', user_detail_api_view, name='usuario_detail_api_view'),
 ]
